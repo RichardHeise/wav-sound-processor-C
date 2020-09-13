d38555113dc0484d23f1debbc3c206aa4dd75335
Richard Heise <rfhf19@inf.ufpr.br>
2020-09-13 17:20:41 -0300

    All done
    Signed-off-by: Richard Heise <rfhf19@inf.ufpr.br>

--- wav.c
+++ wav.c
@@ -1,2 +1,40 @@
 #include "wav.h"
 
+/**
+ * Function to write audio data. 
+ * Receives a s_wav struct by reference (&s_wav).
+ * Receives a file pointer as the output file.
+ */
+void writeAudioData(wav_t *wav_pointer, FILE *output) {
+    fwrite(&wav_pointer->header, sizeof(wav_pointer->header), 1, output);
+    fwrite(wav_pointer->audio,sizeof(int16_t),wav_pointer->header.data.SubChunk2Size/2, output);
+}
+
+/**
+ * Function to read audio data. 
+ * Receives a s_wav struct by reference (&s_wav).
+ * Receives a file pointer as the input file.
+ */
+void readAudioData(wav_t *wav_pointer,FILE *input) {
+
+    fread(&wav_pointer->header, sizeof(wav_pointer->header), 1, input);
+    wav_pointer->audio = malloc(sizeof(int16_t) * wav_pointer->header.data.SubChunk2Size/2);
+    fread(wav_pointer->audio, 2, wav_pointer->header.data.SubChunk2Size, input);
+}
+
+/**
+ * Function to change audio volume. 
+ * Receives a s_wav struct by reference (&s_wav).
+ * Receives a float volume level value.
+ */
+void changeVol(wav_t *wav_pointer, float lvl) {
+    int i;
+    for(i=0; i < wav_pointer->header.data.SubChunk2Size/2; i++) {
+        wav_pointer->audio[i] = wav_pointer->audio[i] * lvl;
+        if (wav_pointer->audio[i] > 32767) {
+            wav_pointer->audio[i] = 32767;
+        } else if (wav_pointer->audio[i] < -32767) {
+            wav_pointer->audio[i] = -32767;
+        }
+    }
+}
--- wavvol.c
+++ wavvol.c
@@ -1,32 +1,9 @@
 #include "wav.h"
 
-void readAudioData(wav_t *wav_pointer,FILE *src) {
-
-    fread(&wav_pointer->header, sizeof(wav_pointer->header), 1, src);
-    wav_pointer->audio = malloc(sizeof(int16_t) * wav_pointer->header.data.SubChunk2Size/2);
-    fread(wav_pointer->audio, 2, wav_pointer->header.data.SubChunk2Size, src);
-}
-
-void changeVol(wav_t *wav_pointer, float lvl) {
-    int i;
-    for(i=0; i < wav_pointer->header.data.SubChunk2Size/2; i++) {
-        wav_pointer->audio[i] = wav_pointer->audio[i] * lvl;
-        if (wav_pointer->audio[i] > 32767) {
-            wav_pointer->audio[i] = 32767;
-        } else if (wav_pointer->audio[i] < -32767) {
-            wav_pointer->audio[i] = -32767;
-        }
-    }
-}
-/*
-void writeAudioData(wav_t *wav_pointer) {
-    fwrite(wav_pointer->header, sizeof(wav_pointer->header), 1, stdout);
-    fwrite(wav_pointer->audio,2,wav_pointer->header.data.SubChunk2Size,stdout);
-}
-*/
 int main (int argc, char **argv) {
     
     FILE *file_input = stdin;
+    FILE *file_output = stdout;
     float level = 1.0;
     wav_t wav;
     int option;
@@ -34,32 +11,38 @@ int main (int argc, char **argv) {
     while ((option = getopt (argc, argv, "i:o:l:")) != -1) {
         float parsed_arg;
         switch (option) {
+
             case 'i':      
                 file_input = fopen(optarg, "r");
+                if (file_input = NULL) {
+                    fprintf(stderr, "Couldn't open file %s", optarg);
+                    exit(-1);
+                }
                 break;
+
             case 'o':
+                file_output = fopen(optarg, "a");
                 break;
+
             case 'l':
                 parsed_arg = atof(optarg);
                 if (parsed_arg <= 10.0 && parsed_arg >= 0.0) {
                     level = parsed_arg;
-                } else {
-                    fprintf(stderr, "Level flag must have a value bigger than 0.0 and lesser than 10.0\n");
-                    exit(-2);
+                    break;
                 }
-                break;
+
+                fprintf(stderr, "Level flag must have a value bigger than 0.0 and lesser than 10.0\n");
+                exit(-2);
+
             default:
                 fprintf (stderr, "Usage: %s -l num -i input -o output\n", argv[0]);
-	            exit(-1);
+	            exit(-3);
         }
     }
 
     readAudioData(&wav, file_input);
     changeVol(&wav, level);
-    // writeAudioData(&wav);
-
-    //fwrite(&wav.header, 44, 1, stdout);
-    //fwrite(&wav.audio,2,wav.header.data.SubChunk2Size,stdout);
+    writeAudioData(&wav, file_output);
 
 
     return 1;
